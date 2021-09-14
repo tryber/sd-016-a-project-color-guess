35ebd6f2401918f948c172bd7664593b12fb18e7
arctavares <cosilk@hotmail.com>
2021-09-13 22:51:33 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,14 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+let array = [];
+
+for(let i = 0; i < 3; i += 1){
+    array.push(getRandomInt(0, 255));
+}
+
+const text = document.getElementById('rgb-color');
+text.innerText = `(${array[0]}, ${array[1]}, ${array[2]})`;
