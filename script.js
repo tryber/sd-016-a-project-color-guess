c5fec4263f712d923bb875ede8f9e6ea37ac2bec
Gustavo Meira <gmeira.antonio@gmail.com>
2021-09-14 15:10:36 -0300

    Quinto requisito

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const balls = document.querySelectorAll('.ball');
+const colorToGuess = document.querySelector('#rgb-color');
+const answerText = document.querySelector('#answer');
 
 function returnRandomRGBColor() {
   const r = Math.floor(Math.random() * 255) + 1;
   const g = Math.floor(Math.random() * 255) + 1;
   const b = Math.floor(Math.random() * 255) + 1;
-  return `rgb(${r}, ${g}, ${b})`
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 function addCorToBalls() {
@@ -13,4 +15,30 @@ function addCorToBalls() {
   });
 }
 
-window.onload = addCorToBalls;
+function pickingRandomColorToGuess() {
+  choosenColor = Math.floor(Math.random() * balls.length);
+  colorToGuess.innerText = balls[choosenColor].style.backgroundColor;
+}
+
+function testBackgroundColor(event) {
+  const currentBall = event.target;
+  if (colorToGuess.innerText === currentBall.style.backgroundColor) {
+    answerText.innerText = 'Acertou!';
+  } else {
+    answerText.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+function addingEventListenerToBalls() {
+  balls.forEach((ball) => {
+    ball.addEventListener('click', testBackgroundColor)
+  });
+}
+
+function setupGame() {
+  addCorToBalls();
+  pickingRandomColorToGuess();
+  addingEventListenerToBalls();
+}
+
+window.onload = setupGame;
