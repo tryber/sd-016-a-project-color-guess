bcb0cb13ecfe396d41764480e8d6d9bfed5070d6
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-09-14 22:33:41 -0300

    adicionado requisito 5

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const rgbColor = document.querySelector('#rgb-color');
 const rgbAnswerSection = document.querySelector('#rgb-answers');
+const answerText = document.querySelector('#answer');
 
 function randomRGB() {
   const r = Math.ceil(Math.random() * 256);
   const g = Math.ceil(Math.random() * 256);
   const b = Math.ceil(Math.random() * 256);
 
-  return [r, g, b];
+  return [`${r}, ${g}, ${b}`];
 }
 
 function rgbReference() {
@@ -33,11 +34,24 @@ function createAnswerColors() {
   return colors;
 }
 
+const answerOption = document.getElementsByClassName('ball');
+
 function insertBackgroundColor() {
   const colorArrayBgC = createAnswerColors();
-  const answerOption = document.querySelectorAll('.ball');
   for (let index = 0; index < answerOption.length; index += 1) {
     answerOption[index].style.backgroundColor = colorArrayBgC[index];
   }
 }
 insertBackgroundColor();
+
+function clickEvent() {
+  const asdadas = document.querySelector('#rgb-answers');
+  asdadas.addEventListener('click', (content) => {
+    const answerUser = content.target;
+    if (answerUser.style.backgroundColor === rgbColor.innerText) {
+      answerText.innerText = 'Acertou!';
+    } else {
+      answerText.innerText = 'Errou! Tente novamente!';
+    }
+  });
+} clickEvent();
