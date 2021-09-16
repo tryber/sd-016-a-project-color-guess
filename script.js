ea8b8eadc9d1d5a4813396c5c345d5b3d4ed7843
alt-art <pedromendescraft@gmail.com>
2021-09-16 00:37:37 -0300

    Add colors to guess

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const colorDisplay = document.querySelector('#rgb-color');
-
-function randomColor() {
-    return `(${randomRange(0, 255)}, ${randomRange(0, 255)}, ${randomRange(0, 255)})`
-}
+const balls = document.querySelectorAll('.ball');
 
 function randomRange(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function randomColor() {
+  return `(${randomRange(0, 255)}, ${randomRange(0, 255)}, ${randomRange(0, 255)})`;
+}
+
+balls.forEach((elem) => {
+  elem.style.backgroundColor = `rgb${randomColor()}`;
+});
 
 const colorGuess = randomColor();
 colorDisplay.innerText = colorGuess;
+const randomBall = balls[Math.floor(Math.random() * balls.length)];
+randomBall.style.backgroundColor = `rgb${colorGuess}`;
+randomBall.classList.toggle('color');
