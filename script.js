316db57f599928486bcddf17682878b66af9a26b
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-09-17 15:12:29 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 const getIdRgb = document.querySelector('#rgb-color');
-const rgbColors = ['(168,3,1)'];
 
-function chooseColors() {
-  const choose = rgbColors[Math.floor(Math.random() * 1)];
-  return choose;
+const getSection = document.getElementsByTagName('section')[0];
+const childIdRgb = document.createElement('p');
+getSection.appendChild(childIdRgb);
+
+function creatingRgbColor() {
+  const choose = Math.floor(Math.random() * 255);
+  const choose1 = Math.floor(Math.random() * 255);
+  const choose2 = Math.floor(Math.random() * 255);
+  // `` backticks - criação do template
+  // concatena strings e variáveis - $
+  const rgbColor = `rgb(${choose}, ${choose1}, ${choose2})`;
+  return rgbColor;
 }
 
-function inputRgb() {
-  getIdRgb.innerHTML = chooseColors();
+function getInputRgbAndDisplayInMyPage() {
+  const rgbNumber = creatingRgbColor();
+  getIdRgb.innerText = rgbNumber.replace('rgb', '');
 }
+getInputRgbAndDisplayInMyPage();
 
-inputRgb();
+function createColors() {
+  const getBall = document.querySelectorAll('.ball');
+  for (let index = 0; index < 6; index += 1) {
+    getBall[index].style.backgroundColor = creatingRgbColor();
+  }
+  getInputRgbAndDisplayInMyPage();
+}
 
 function createSpans() {
   for (let index = 1; index <= 6; index += 1) {
-    const createSection = document.createElement('span');
-    createSection.classList.add('ball');
-    createSection.style.padding = '20px';
-    createSection.style.borderRadius = '50%';
-    createSection.style.height = '1px';
-    createSection.style.border = '1px solid black';
-    createSection.style.width = '1px';
-    createSection.style.marginLeft = '10px';
-    createSection.style.marginTop = '100px';
-    createSection.style.display = 'inline-block';
-    getIdRgb.appendChild(createSection);
+    const createSpan = document.createElement('span');
+    createSpan.classList.add('ball');
+    createSpan.style.padding = '20px';
+    createSpan.style.borderRadius = '50%';
+    createSpan.style.height = '1px';
+    createSpan.style.border = '1px solid black';
+    createSpan.style.width = '1px';
+    createSpan.style.marginLeft = '10px';
+    createSpan.style.marginTop = '5px';
+    createSpan.style.display = 'inline-block';
+    childIdRgb.appendChild(createSpan);
   }
+  createColors();
 }
-
 createSpans();
