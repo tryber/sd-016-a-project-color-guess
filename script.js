6fa50234f7e36880112b28d0b5c47200b6b7eea5
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2021-09-18 00:11:09 -0300

    terceiro ao quinto requisito

--- script.js
+++ script.js
@@ -8,10 +8,61 @@ function randomColors() {
   return colorSelected;
 }
 
+function arrayColor() {
+  const arrayColors = [];
+  for (let index = 0; index < 6; index += 1) {
+    arrayColors.push(randomColors());
+  }
+  return arrayColors;
+}
+
+const generatorColor = arrayColor();
+console.log(generatorColor);
+
 function createGamecolor() {
+  const randon = Math.floor(Math.random() * generatorColor.length);
+  console.log(randon);
   const createColor = document.createElement('p');
-  createColor.innerHTML = randomColors();
+  createColor.id = 'rgb-color';
+  createColor.innerHTML = generatorColor[randon];
   container.appendChild(createColor);
 }
 
+function createOptionColors() {
+  for (let index = 0; index < 6; index += 1) {
+    // let sort = Math.floor(Math.random() * 6)
+    const createDivColors = document.createElement('div');
+    createDivColors.classList.add('ball');
+    const rgb = `rgb${generatorColor[index]}`;
+    createDivColors.style.backgroundColor = rgb;
+    container.appendChild(createDivColors);
+  }
+}
+
+function createAnswer() {
+  const creatText = document.createElement('p');
+  creatText.id = 'answer';
+  creatText.innerHTML = 'Escolha uma cor';
+  container.appendChild(creatText);
+}
+
+function validationColor() {
+  const selectAnswer = document.querySelector('#answer');
+  const selectColor = document.querySelector('#rgb-color').innerHTML;
+  const selectBalls = document.querySelectorAll('.ball');
+  for (let index = 0; index < selectBalls.length; index += 1) {
+    selectBalls[index].addEventListener('click', (event) => {
+      console.log(event.target.style.backgroundColor);
+      if (event.target.style.backgroundColor === `rgb${selectColor}`) {
+        selectAnswer.innerHTML = 'Acertou!';
+      } else {
+        selectAnswer.innerHTML = 'Errou! Tente novamente!';
+      }
+    });
+  }
+}
+
 createGamecolor();
+createOptionColors();
+createAnswer();
+validationColor();
