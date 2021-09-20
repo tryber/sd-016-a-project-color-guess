a8d9184616f4913e5d2c8ddfaaca99e0912154ed
Fernanda9421 <fer.candrade21@gmail.com>
2021-09-20 14:10:45 -0300

    Requisitos 3 e 4 - Adiciona circulos de cores geradas automaticamente pelo JS

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const standardColor = () => {
   colorRGB.appendChild(createParagraph);
 };
 standardColor();
+
+const createColorBall = () => {
+  const ballContainer = document.querySelector('#color-ball');
+  for (let index = 1; index <= 6; index += 1) {
+    const createDivBall = document.createElement('div');
+    createDivBall.classList.add('ball');
+    ballContainer.appendChild(createDivBall);
+  }
+};
+createColorBall();
+
+const backgroundColorBall = () => {
+  const ballSelected = document.querySelectorAll('.ball');
+  for (let index = 0; index < ballSelected.length; index += 1) {
+    const currentBall = ballSelected[index];
+    const colorR = Math.floor(Math.random() * 256);
+    const colorG = Math.floor(Math.random() * 256);
+    const colorB = Math.floor(Math.random() * 256);
+    currentBall.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
+  }
+};
+backgroundColorBall();
