37c04814107973a89812b5f4ad17a39c17afab59
Fernanda9421 <fer.candrade21@gmail.com>
2021-09-21 10:49:04 -0300

    Requisito 5 - Texto indicando se a resposta esta correta ou nao

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ createColorBall();
 
 const backgroundColorBall = () => {
   const ballSelected = document.querySelectorAll('.ball');
+  const generateColor = document.querySelector('#rgb-color');
   for (let index = 0; index < ballSelected.length; index += 1) {
     const currentBall = ballSelected[index];
     const colorR = Math.floor(Math.random() * 256);
@@ -26,5 +27,33 @@ const backgroundColorBall = () => {
     const colorB = Math.floor(Math.random() * 256);
     currentBall.style.backgroundColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
   }
+  const randomColor = Math.floor(Math.random() * 5);
+  generateColor.innerHTML = ballSelected[randomColor].style.backgroundColor;
 };
 backgroundColorBall();
+
+const createParagraphAnswer = () => {
+  const sectionAnswer = document.querySelector('#paragraphAnswer');
+  const paragraphAnswer = document.createElement('p');
+  paragraphAnswer.id = 'answer';
+  paragraphAnswer.innerText = 'Escolha uma cor';
+  sectionAnswer.appendChild(paragraphAnswer);
+};
+createParagraphAnswer();
+
+const ballSelectedClass = () => {
+  const balls = document.querySelectorAll('.ball');
+  const colorRGB = document.querySelector('#rgb-color').innerText;
+  const paragraphAnswer = document.querySelector('#answer');
+
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].addEventListener('click', (event) => {
+      if (colorRGB === event.target.style.backgroundColor) {
+        paragraphAnswer.innerText = 'Acertou!';
+      } else {
+        paragraphAnswer.innerText = 'Errou! Tente novamente!';
+      }
+    });
+  }
+};
+ballSelectedClass();
