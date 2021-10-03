de2a03c6610c8925fe7532746f9472966ec3448a
baky2708 <flavioisaque123@gmail.com>
2021-10-03 17:21:05 -0300

    05 requeriment - Complete

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const txtColor = document.querySelector('#rgb-color');
 const ballColors = document.querySelectorAll('.ball');
+const answer = document.querySelector('#answer');
+const correct = Math.floor(Math.random() * 6);
 
-txtColor.innerHTML = '(168, 34, 1)';
+function random() {
+  return Math.floor(Math.random() * 255) + 1;
+}
+
+console.log(correct);
+
+txtColor.innerHTML = `(${random()}, ${random()}, ${random()})`;
+ballColors[correct].style.backgroundColor = `rgb${txtColor.innerText}`;
+
+for (let i = 0, len = ballColors.length; i < len; i += 1) {
+  ballColors[i].addEventListener('click', (event) => {
+    if (event.target.style.backgroundColor === `rgb${txtColor.innerText}`) {
+      answer.innerHTML = 'Acertou!';
+    } else {
+      answer.innerHTML = 'Errou! Tente novamente!';
+    }
+  });
+}
 
 window.onload = () => {
   for (let i = 0, len = ballColors.length; i < len; i += 1) {
-    const rgb1 = Math.floor(Math.random() * 255 + 1);
-    const rgb2 = Math.floor(Math.random() * 255 + 1);
-    const rgb3 = Math.floor(Math.random() * 255 + 1);
-    ballColors[i].style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+    if (ballColors[i].style.backgroundColor === '') {
+      const rgb1 = random();
+      const rgb2 = random();
+      const rgb3 = random();
+      ballColors[i].style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+    }
   }
 };
