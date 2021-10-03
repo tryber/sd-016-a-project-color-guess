915a2a1d5cc35ece3daa71c189b2441f26661925
baky2708 <flavioisaque123@gmail.com>
2021-10-03 18:54:02 -0300

    06 requeriment - Complete

--- script.js
+++ script.js
@@ -2,27 +2,28 @@ const txtColor = document.querySelector('#rgb-color');
 const ballColors = document.querySelectorAll('.ball');
 const answer = document.querySelector('#answer');
 const correct = Math.floor(Math.random() * 6);
+const btnReset = document.querySelector('#reset-game');
 
 function random() {
   return Math.floor(Math.random() * 255) + 1;
 }
 
-console.log(correct);
-
-txtColor.innerHTML = `(${random()}, ${random()}, ${random()})`;
-ballColors[correct].style.backgroundColor = `rgb${txtColor.innerText}`;
-
-for (let i = 0, len = ballColors.length; i < len; i += 1) {
-  ballColors[i].addEventListener('click', (event) => {
-    if (event.target.style.backgroundColor === `rgb${txtColor.innerText}`) {
-      answer.innerHTML = 'Acertou!';
-    } else {
-      answer.innerHTML = 'Errou! Tente novamente!';
-    }
-  });
+function logicGame() {
+  answer.innerHTML = 'Escolha uma cor';
+  txtColor.innerHTML = `(${random()}, ${random()}, ${random()})`;
+  ballColors[correct].style.backgroundColor = `rgb${txtColor.innerText}`;
+  for (let i = 0, len = ballColors.length; i < len; i += 1) {
+    ballColors[i].addEventListener('click', (event) => {
+      if (event.target.style.backgroundColor === `rgb${txtColor.innerText}`) {
+        answer.innerHTML = 'Acertou!';
+      } else {
+        answer.innerHTML = 'Errou! Tente novamente!';
+      }
+    });
+  }
 }
 
-window.onload = () => {
+function paintBalls() {
   for (let i = 0, len = ballColors.length; i < len; i += 1) {
     if (ballColors[i].style.backgroundColor === '') {
       const rgb1 = random();
@@ -31,4 +32,13 @@ window.onload = () => {
       ballColors[i].style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
     }
   }
-};
+}
+
+function game() {
+  paintBalls();
+  logicGame();
+}
+
+game();
+
+btnReset.addEventListener('click', game);
