1628037f89644666c19c2165f09dee4c981911d1
baky2708 <flavioisaque123@gmail.com>
2021-10-03 16:30:12 -0300

    04 requeriment - Complete

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const txtColor = document.querySelector('#rgb-color');
+const ballColors = document.querySelectorAll('.ball');
 
 txtColor.innerHTML = '(168, 34, 1)';
+
+window.onload = () => {
+  for (let i = 0, len = ballColors.length; i < len; i += 1) {
+    const rgb1 = Math.floor(Math.random() * 255 + 1);
+    const rgb2 = Math.floor(Math.random() * 255 + 1);
+    const rgb3 = Math.floor(Math.random() * 255 + 1);
+    ballColors[i].style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+  }
+};
