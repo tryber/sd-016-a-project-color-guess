63dbb248474150fdd1ceeea56b23a32b6b692707
joao <jgsspinelli92@gmail.com>
2021-10-10 19:01:40 -0300

    entrega dos requisitos 5, 6, 7

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const balls = document.querySelectorAll('.ball');
 const color = document.getElementById('rgb-color');
+const btn = document.getElementById('reset-game');
+const answer = document.getElementById('answer');
+const scoreBoard = document.getElementById('score');
 
 const coloringBalls = () => {
   for (let index = 0; index < balls.length; index += 1) {
@@ -20,6 +23,40 @@ const rightColor = () => {
 const matchColorWithBall = () => {
   balls[Math.floor(Math.random() * 6)].style.backgroundColor = `rgb${color.innerText}`;
 };
+
+const rightOrWrong = (click) => {
+  answer.innerText = 'Escolha uma cor';
+  if (click.target.style.backgroundColor === `rgb${color.innerText}`) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente.';
+  }
+};
+
+const score = (click) => {
+  let scoreToNumber = parseInt(scoreBoard.innerText, 10);
+  if (click.target.style.backgroundColor === `rgb${color.innerText}`) {
+    console.log(scoreToNumber);
+    scoreToNumber += 3;
+    console.log(scoreToNumber);
+  }
+  scoreBoard.innerText = scoreToNumber;
+  return scoreBoard;
+};
+
+for (let index = 0; index < balls.length; index += 1) {
+  balls[index].addEventListener('click', rightOrWrong);
+  balls[index].addEventListener('click', score);
+}
+
+const resetGame = () => {
+  answer.innerText = 'Escolha uma cor';
+};
+
+btn.addEventListener('click', rightColor);
+btn.addEventListener('click', coloringBalls);
+btn.addEventListener('click', matchColorWithBall);
+btn.addEventListener('click', resetGame);
 
 rightColor();
 coloringBalls();
